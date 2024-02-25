02658b028bdac258478355af9c4c51960b988a25
rafaelpilger <pilgerrafael772@gmail.com>
2024-02-25 19:40:31 -0300

    atividade_1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+function incrementarOcontador() {
+  contador.value++;
+}
+
+function DecrementarOcontador() {
+  if (contador.value > 0){
+     contador.value--;
+  }
+}
+
+function zeradorDOcontador() {
+  contador.value = 0;
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="botoes">
+  <button @click="zeradorDOcontador">zerar</button>
+  <button @click="incrementarOcontador">Incrementar</button>
+  </div>
+  <button @click="DecrementarOcontador">Decrementar</button>
+  <div class="zerador">
+  <p>Valor do contador è : {{ contador }}</p>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  button{
+    font-weight: bold;
+    border-radius: 30px;
+    border-color: aqua;
+    color: blue;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  p{
+    color: red;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
